ba9c64550f4e30274ac912fbba4a29f05b632844
Jake Cheek <jake@f-m.fm>
2020-03-06 13:00:33 -0500

    add post id to user

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -7,6 +7,8 @@ const defaultState = UserMap()
 export default handleActions(
   {
     ADD_USER: (state, { payload: { user } }) => state.set(user.id, user),
+    ADD_POST: (state, { payload: { post } }) =>
+      state.updateIn([post.userId, 'postIds'], ids => ids.add(post.id)),
     ADD_COMMENT: (state, { payload: { comment } }) =>
       state.updateIn([comment.userId, 'commentIds'], ids =>
         ids.add(comment.id)
