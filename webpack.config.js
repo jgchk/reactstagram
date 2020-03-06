78213374f511fd169061003f84660eb8bcd4d3a9
Jake Cheek <jake@f-m.fm>
2020-03-05 19:09:53 -0500

    set up babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ const dev = process.env.NODE_ENV === "development";
 module.exports = {
   mode: dev ? "development" : "production",
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
