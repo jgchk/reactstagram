64821ee489892a51850bb506a2e34d30bc3e1df2
Jake Cheek <jake@f-m.fm>
2020-03-06 12:58:29 -0500

    add comment ids to user and post

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -7,6 +7,10 @@ const defaultState = PostMap()
 export default handleActions(
   {
     ADD_POST: (state, { payload: { post } }) => state.set(post.id, post),
+    ADD_COMMENT: (state, { payload: { comment } }) =>
+      state.updateIn([comment.postId, 'commentIds'], ids =>
+        ids.add(comment.id)
+      ),
   },
   defaultState
 )
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -7,6 +7,10 @@ const defaultState = UserMap()
 export default handleActions(
   {
     ADD_USER: (state, { payload: { user } }) => state.set(user.id, user),
+    ADD_COMMENT: (state, { payload: { comment } }) =>
+      state.updateIn([comment.userId, 'commentIds'], ids =>
+        ids.add(comment.id)
+      ),
   },
   defaultState
 )
