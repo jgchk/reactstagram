f7bb584eddbcfbecbf52b9e7826c3057fb8ea76e
Jake Cheek <jake@f-m.fm>
2020-03-07 18:34:42 -0500

    fix plurality of likes text

--- src/components/PostLikes/index.jsx
+++ src/components/PostLikes/index.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.module.less'
 
-const PostLikes = ({ likes }) => (
-  <div className={styles.likes}>{`${likes} likes`}</div>
-)
+const PostLikes = ({ likes }) => {
+  let suffix = 'like'
+  if (Math.abs(likes) !== 1) suffix += 's'
+  return <div className={styles.likes}>{`${likes} ${suffix}`}</div>
+}
 
 PostLikes.propTypes = {
   likes: PropTypes.number.isRequired,
